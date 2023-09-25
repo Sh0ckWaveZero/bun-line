28f31457f9d575486e809d765248876e3980c3fc
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-09-25 21:42:52 +0700

    🔧 chore(next.config.js): remove unused PrismaPlugin import and webpack configuration 🔧 chore(package.json): remove unused @prisma/nextjs-monorepo-workaround-plugin dependency

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-
-    return config
-  },
   runtime: 'experimental-edge',
 }
 
