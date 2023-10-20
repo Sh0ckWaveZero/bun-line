6f20ff4e54638822ba727a21cf37ec01aa146f91
Aphidet-Hongseetong <ottooffple@icloud.com>
2023-10-20 11:18:37 +0700

    🔧 chore(next.config.js): add support for dynamic import of '/lib/prisma.ts' to improve performance and flexibility

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
  */
 const nextConfig = {
   runtime: 'edge', // for Edge API Routes only
+  unstable_allowDynamic: [
+    // allows a single file
+    '/lib/prisma.ts',
+  ],
 }
 
 module.exports = nextConfig
